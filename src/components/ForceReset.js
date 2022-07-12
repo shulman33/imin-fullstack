297884609460ec86e5d43345bb5b0a71d72c9e46
shulman33 <sshulma5@mail.yu.edu>
2022-07-12 15:29:59 -0400

    redirects to a change password page after signup

---
+++ src/components/ForceReset.js
@@ -0,0 +1,134 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import logo from "../assests/ImIn-logos/ImIn-logos.jpeg";
+import {Auth} from "aws-amplify";
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+import Alert from '@mui/material/Alert';
+
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const theme = createTheme();
+
+export default function ForceReset({setIsLoggedIn}) {
+    const [username, setUsername] = useState('')
+    const [temp, setTemp] = useState('')
+    const [newPassword, setNewPassword] = useState('')
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        Auth.signIn(username, temp)
+            .then(user => {
+                if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
+                    const { requiredAttributes } = user.challengeParam;
+                    Auth.completeNewPassword(
+                        user,
+                        newPassword,
+                    ).then(user => {
+                        // at this time the user is logged in if no MFA required
+                        console.log(user);
+                        setIsLoggedIn(user);
+                        localStorage.setItem('user', JSON.stringify(user))
+                        navigate("/bot")
+                    }).catch(e => {
+                        console.log(e);
+                        setErrorMessage('Make sure you entered the same email address you used to signup. \n Make sure you entered the temporary password correctly. \n Your new password needs to contain at least one number and have a minimum of 8 characters.');
+                    });
+                } else {
+                    // other situations
+                }
+            }).catch(e => {
+            console.log(e);
+        });
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <img alt='ImIn Logo' src={logo} style={{width : '15vh', marginBottom : '2vh', borderRadius : '50%'}}/>
+                    <Typography component="h1" variant="h5">
+                        Change Password
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        {!errorMessage && <Alert severity="info">Check your email for the temporary password we sent you</Alert>}
+                        {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="email"
+                            label="Email"
+                            type="input"
+                            id="email"
+                            autoComplete="current-email"
+                            onChange={e => setUsername(e.target.value)}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="temp"
+                            label="Temporary Password"
+                            type="password"
+                            id="temp"
+                            autoComplete="temp-password"
+                            onChange={e => setTemp(e.target.value)}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="New Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onChange={e => setNewPassword(e.target.value)}
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Change Password
+                        </Button>
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 8, mb: 4 }} />
+            </Container>
+        </ThemeProvider>
+    );
+}
+
+
--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -2,22 +2,26 @@ import {loadStripe} from '@stripe/stripe-js';
 import React from "react";
 import Link from "@mui/material/Link";
 
+
 export default function Subscribe() {
     const handleClick = async (e) => {
-        const stipe = await loadStripe('pk_live_51LHViCE10R7clMGPE8vuPuyigHV2lFRBUa40PGjEJMiCztmySv5D1P2g1mStW2d6GLCS8Z0behHpB27r1IljYbZ9001Q0xK2bg');
+        const stipe = await loadStripe('pk_test_51LHViCE10R7clMGPar467buonYNVnMYD3hpzm2FJ9V73TaQCdqm2w2bl5ymOXcepGaJaACc3fS0zLtSkGTqfTeyi00c3p5Z25Y');
+        // live api key
+        // const stipe = await loadStripe('pk_live_51LHViCE10R7clMGPE8vuPuyigHV2lFRBUa40PGjEJMiCztmySv5D1P2g1mStW2d6GLCS8Z0behHpB27r1IljYbZ9001Q0xK2bg');
         const {error} = await stipe.redirectToCheckout({
             lineItems: [{
-                price: 'price_1LHW1gE10R7clMGPZrl1hT6o',
+                price: 'price_1LHWT1E10R7clMGPqANHPWaw',
+                // production price
+                // price: 'price_1LHW1gE10R7clMGPZrl1hT6o',
                 quantity: 1
             }],
             mode: 'payment',
-            successUrl: 'https://imin.site',
+            successUrl: 'http://localhost:3000/resetpassword',
             // should act config this page
-            cancelUrl: 'https://imin.site'
+            cancelUrl: 'http://localhost:3000/'
         })
     }
     return (
-        // <button onClick={handleClick}>Sign Up</button>
         <Link onClick={handleClick} variant="body2" style={{cursor: 'pointer'}}>
             {"Don't have an account? Sign Up"}
         </Link>
