37c0e7bdb7ccc9af12e0b72ce164ffac74c6eded
shulman33 <sshulma5@mail.yu.edu>
2023-02-02 13:28:18 -0500

    added menu dropdown

--- src/components/RegistrationPage.js
+++ src/components/RegistrationPage.js
@@ -1,12 +1,13 @@
 //בס׳ד
 import {useEffect, useState} from "react";
+import * as React from 'react';
 import logo from "../assests/ImIn-logos/ImIn-logos_black.png"
 import '../styles/regipage.css';
 import {Auth} from "aws-amplify";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from "@mui/material/Button";
-import {AppBar, CircularProgress, Toolbar} from "@mui/material";
+import {AppBar, Avatar, CircularProgress, Menu, MenuItem, Toolbar} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import {useNavigate} from "react-router-dom";
 import Alert from '@mui/material/Alert';
@@ -15,6 +16,9 @@ import axios from 'axios';
 import { S3 } from 'aws-sdk';
 import '../aws-config';
 import Help from "./HelpIcon";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from '@mui/icons-material/Menu';
+import {AccountCircle} from "@mui/icons-material";
 
 let api = 'https://pgclq90efg.execute-api.us-east-1.amazonaws.com/beta/schedule-registration'
 
@@ -212,6 +216,15 @@ function RegistrationPage({logout}) {
 
     console.log(values)
 
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
 
     return (
         <div>
@@ -220,7 +233,60 @@ function RegistrationPage({logout}) {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         ImIn
                     </Typography>
-                    <Button color="inherit" onClick={handleLogout}>Logout</Button>
+                    <React.Fragment>
+                        <IconButton
+                            onClick={handleClick}
+                            size="small"
+                            sx={{ ml: 2 }}
+                            aria-controls={open ? 'account-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                        >
+                            <AccountCircle sx={{ width: 32, height: 32 }}/>
+                        </IconButton>
+                        <Menu
+                            anchorEl={anchorEl}
+                            id="account-menu"
+                            open={open}
+                            onClose={handleClose}
+                            onClick={handleClose}
+                            PaperProps={{
+                                elevation: 0,
+                                sx: {
+                                    overflow: 'visible',
+                                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                    mt: 1.5,
+                                    '& .MuiAvatar-root': {
+                                        width: 32,
+                                        height: 32,
+                                        ml: -0.5,
+                                        mr: 1,
+                                    },
+                                    '&:before': {
+                                        content: '""',
+                                        display: 'block',
+                                        position: 'absolute',
+                                        top: 0,
+                                        right: 14,
+                                        width: 10,
+                                        height: 10,
+                                        bgcolor: 'background.paper',
+                                        transform: 'translateY(-50%) rotate(45deg)',
+                                        zIndex: 0,
+                                    },
+                                },
+                            }}
+                            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                        >
+                            <MenuItem onClick={handleClose}>
+                                <Button color="inherit" onClick={handleLogout}>Logout</Button>
+                            </MenuItem>
+                            <MenuItem onClick={handleClose}>
+                                <Button color="inherit">Cancel Subscription</Button>
+                            </MenuItem>
+                        </Menu>
+                    </React.Fragment>
                 </Toolbar>
             </AppBar>
             {alert &&
